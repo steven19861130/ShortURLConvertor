5179437222966040f95ad5fa6c947b55dfa133f2
I078076 <steven.yang@sap.com>
2020-11-01 16:47:33 +0800

    add swagger

--- src/main/java/com/steven/shorturldemo/ShorturldemoApplication.java
+++ src/main/java/com/steven/shorturldemo/ShorturldemoApplication.java
@@ -2,8 +2,10 @@ package com.steven.shorturldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ShorturldemoApplication {
 
     public static void main(String[] args) {
